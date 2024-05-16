da8fd695958c6cae06f0a8b2a3d1761c13c42c0e
holy-heart <kickshamahaneverdie@gmail.com>
2024-05-16 17:08:42 +0100

    Terliné

--- execute.py
+++ execute.py
@@ -4,7 +4,9 @@ import cv2
 import os
 import numpy as np
 #pip install Flask opencv-python-headless scipy numpy
+#il suffit d'executer ce script python et d'entrer l'url qui s'affiche dans le terminal sur un navigateur
 
+#Fonction qui retourne les keypoints de l'iris de chaque image
 def extract_sift_features(image):
     sift = cv2.SIFT_create()
     keypoints, descriptors = sift.detectAndCompute(image, None)
@@ -18,6 +20,8 @@ def extract_sift_features(image):
             D.append(j)
     return K, D
 
+
+#fonction qui retourne les bons matches entre deux images
 def match_features(probleme_d, database_d, ration_distance=0.76):
     bf = cv2.BFMatcher()
     probleme = np.array(probleme_d)
@@ -36,7 +40,7 @@ app=Flask(__name__, static_url_path='/static')
 app.secret_key = 'hmmm'
 
 
-
+#page d'accueil, la focntion recoit l'image
 @app.route("/", methods=['POST', "GET"])
 def home():
     if request.method == "POST":
@@ -50,12 +54,13 @@ def home():
         return render_template("ident.html")
 
 
-
+#page Enregistrer une image dans la bdd
 @app.route("/insert", methods=['POST', "GET"])
 def insert():
     if request.method == "POST":
         image_i=request.files['img_i']
         image_i=image_i.read()
+
         with open(f'database1/sign.png', "wb") as f:
             f.write(image_i)
         flash("La photo a été ajoutée à la base de données avec succès. Vous pouvez l'identifier sur la page d'accueil, en haut à gauche, avec une autre photo du même œil (ident.png).", "success")
@@ -66,27 +71,30 @@ def insert():
 
 
 
+#code pour l'identification
 @app.route("/solution/")
 def solution():
     
     SEUIL = 100 
     probleme_c = cv2.imread("static/probleme.png",cv2.IMREAD_GRAYSCALE)
-    probleme_c = cv2.equalizeHist(probleme_c)
-    cv2.imwrite("static/probleme.png", probleme_c)
+    probleme_c = cv2.equalizeHist(probleme_c)#augmente le contrast
+    cv2.imwrite("static/probleme.png", probleme_c)#la bibliotheque flask demande l'utilisation d'un fichier static
+    #bdd
     database_images = []
     for file in sorted(os.listdir("database1")):
         database_images.append(os.path.join("database1", file))
 
     probleme_k, probleme_d = extract_sift_features(probleme_c)
+    
     max_matches = 0
-
+    #comparer l'image inséré avec toutes les images de database1
     for database in database_images:
         image_c2 = cv2.imread(database,cv2.IMREAD_GRAYSCALE)
         database_c = cv2.equalizeHist(image_c2)
         database_k, database_d = extract_sift_features(database_c)
         matches = match_features(probleme_d, database_d)
         print(database,len(matches))
-
+        #on garde la photo avec le maximum de matches
         if len(matches) > max_matches:
             match_sol= matches
             max_matches=len(match_sol)
